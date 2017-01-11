98c8412a5d64f6d5c4b0412ed02a0be4b6ff2b9e
Justin Jones <jj@kickstarter.com>
2017-01-11 00:11:04 -0500

    annotate Proc#kwurry

--- lib/kwurry.rb
+++ lib/kwurry.rb
@@ -3,11 +3,16 @@ require "kwurry/version"
 module Kwurry
   refine Proc do
     def kwurry
-      keys_index = parameters.reduce(Hash.new { |h,k| h[k] = [] }) { |h, (type, name)| h[type] << name; h }
-      keyreqs = keys_index[:keyreq]
-      keyopts = keys_index[:key]
-      keyrest = keys_index[:keyrest].any?
+      # index keyword names by keyword type
+      ki = parameters.reduce(Hash.new { |h, k| h[k] = [] }) { |h, (type, name)| h[type] << name; h }
+      keyreqs = ki[:keyreq]
+      keyopts = ki[:key]
+      keyrest = ki[:keyrest].any?
 
+      # accepts keyword names of all passed arguments
+      # raises error if invalid keyword names supplied
+      # returns true if all required keyword names supplied
+      # returns false otherwise (indicates partially-applied lambda should be returned)
       apply = ->(keys) {
         if !keyrest
           invalid_keys = (keys - (keyreqs + keyopts))
@@ -16,7 +21,11 @@ module Kwurry
         (keyreqs - keys).empty?
       }
 
-      factory = -> (**applied_kwargs) {
+      # accepts any previously passed kwargs
+      # returns a lambda that accepts any kwargs which ...
+      # * returns result of original lambda if should apply
+      # * otherwise invokes factory to return partially-applied curried lambda
+      factory = ->(**applied_kwargs) {
         ->(**kw) {
           kwargs = applied_kwargs.merge(kw)
           return self.(**kwargs) if apply.(kwargs.keys)
@@ -24,6 +33,11 @@ module Kwurry
         }
       }
 
+      # factory and apply are defined in this closure so they can access
+      # keyreqs, keyopts, keyrest
+      # as well as self (original lambda) to eventually invoke it
+
+      # inovke factory (with no args) to curry the lambda
       factory.()
     end
   end
